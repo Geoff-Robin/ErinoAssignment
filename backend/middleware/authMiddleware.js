d7035a7ffd7137943060c80b110fcbfc646773e0
Geoff-Robin <jeffrobin132004@gmail.com>
2025-08-22 19:30:51 +0530

    AUTH DONE

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,11 +3,9 @@ import jwt from "jsonwebtoken";
 const authMiddleware = (req, res, next) => {
   try {
     const token = req.cookies.token;
-
     if (!token) {
       return res.status(401).json({ message: "Unauthorized: No token provided" });
     }
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
