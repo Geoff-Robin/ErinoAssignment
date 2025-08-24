ee044461e91fbcfff432bec0867ed82e052e0972
Geoff-Robin <jeffrobin132004@gmail.com>
2025-08-24 16:19:18 +0530

    edited cookie settings

--- backend/utils/random_stuff.js
+++ backend/utils/random_stuff.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
   
   res.cookie("token", token, {
     httpOnly: true,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
